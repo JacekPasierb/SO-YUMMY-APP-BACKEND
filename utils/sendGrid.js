d1d0c8f0f0894d9f5db58baa642d5c4b6eef9902
Jacek Pasierb <jack_byk@o2.pl>
2023-12-25 10:21:04 +0100

    Fixbug next sendGrid

--- utils/sendGrid.js
+++ utils/sendGrid.js
@@ -1,40 +1,30 @@
-// using Twilio SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
-
 const sgMail = require("@sendgrid/mail");
 require("dotenv").config();
-const send = (email, verificationToken) => {
+
+const send = async (email, verificationToken) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
-    to: email, // Change to your recipient
-    from: "jack_byk@o2.pl", // Change to your verified sender
-    subject: "SO YUMMY APP email verification",
-    text: "and easy to do anywhere, even with Node.js",
+    to: email,
+    from: "jack_byk@o2.pl",
+    subject: "SO YUMMY APP weryfikacja adresu e-mail",
+    text: "Łatwe nawet z użyciem Node.js",
     html: `
-       <div style="text-align: center;">
-       <h1>SO YUMMY APP</h1>
-       <p style="font-size:16px;">Verify your e-mail address by clicking on this link - <a href="https://so-yummy-app-backend.vercel.app/api/users/verify/${verificationToken}" target="_blank" rel="noopener noreferrer nofollow"><strong>Verification Link</strong></a></p>
-       </div>
-       `,
+      <div style="text-align: center;">
+        <h1>SO YUMMY APP</h1>
+        <p style="font-size:16px;">Zweryfikuj swój adres e-mail, klikając w ten link - <a href="https://so-yummy-app-backend.vercel.app/api/users/verify/${verificationToken}" target="_blank" rel="noopener noreferrer nofollow"><strong>Link weryfikacyjny</strong></a></p>
+      </div>
+    `,
   };
 
-//   sgMail
-//     .send(msg)
-//     .then(() => {
-//       console.log("Email sent");
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     });
-// };
-try {
-  const sended = await sgMail.send(msg);
-  console.log("sended: ", sended);
-  return { success: true };
-} catch (error) {
-  console.error(error);
-  return { succes: false };
-}
+  try {
+    const wyslane = await sgMail.send(msg);
+    console.log("E-mail wysłany");
+    return { sukces: true };
+  } catch (error) {
+    console.error(error);
+    return { sukces: false };
+  }
+};
 
 module.exports = {
   send,
