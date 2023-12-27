92f46d5208a20b11eb3e436aa0efffdfd7e4bcb2
Jacek Pasierb <jack_byk@o2.pl>
2023-12-27 23:10:02 +0100

    s

--- controller/userController.js
+++ controller/userController.js
@@ -52,7 +52,7 @@ const register = async (req, res, next) => {
      return res.status(409).json({
        status: "Conflict",
        code: "409",
-       message: `bb${process.env.SECRET}`,
+       message: `cc${process.env.SENDGRID_API_KEY}`,
      });
    }
     handle201(res, "Registration successful", {
