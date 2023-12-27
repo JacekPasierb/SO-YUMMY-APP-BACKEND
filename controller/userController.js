a90d20c6e0f5d2479ac6c2558dd560b4c52e9028
Jacek Pasierb <jack_byk@o2.pl>
2023-12-27 22:19:14 +0100

    S

--- controller/userController.js
+++ controller/userController.js
@@ -52,7 +52,7 @@ const register = async (req, res, next) => {
      return res.status(409).json({
        status: "Conflict",
        code: "409",
-       message: `${checkSend}`,
+       message: checkSend,
      });
    }
     handle201(res, "Registration successful", {
