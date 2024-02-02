b713c30071b23c25eec060aeef94eaeeb4cb9b2a
Jacek Pasierb <jack_byk@o2.pl>
2024-02-02 22:07:47 +0100

    Swagger cant run api-docs on production

--- app.js
+++ app.js
@@ -4,20 +4,27 @@ require("dotenv").config();
 require("./config/passport-config");
 
 const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 const app = express();
-const swaggerDocument = require('./swagger.json'); 
+const swaggerDocument = require("./swagger.json");
 const logger = require("morgan");
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
 const userRouter = require("./routes/api/user");
 const subscribeRouter = require("./routes/api/subscribe.js");
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 const handleError = require("./utils/handleErrors");
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL }));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL })
+);
 app.use("/api/users", userRouter);
 app.use("/api/subscribe", subscribeRouter);
 
