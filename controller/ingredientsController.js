2ed3642e5e823b1f53da9f782343c7ed0fcb9159
Jacek Pasierb <jack_byk@o2.pl>
2024-03-03 21:25:43 +0100

    Route Ingredient add

--- controller/ingredientsController.js
+++ controller/ingredientsController.js
@@ -1,21 +1,38 @@
 const { Ingredient } = require("../models/ingedientModel");
 
 const getAllIngredients = async (req, res, next) => {
-    try {
-        const ingredients = await Ingredient.find();
+  try {
+    const ingredients = await Ingredient.find();
 
-        res.status(200).json({
-            status: "success",
-            code: 200,
-            data: {
-              ingredients
-            },
-          });
-    } catch (error) {
-        next(error)
-    }
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      data: {
+        ingredients,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getIngredientById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const ingredient = await Ingredient.findById(id);
+    res.status(200).json({
+      status: "success",
+      code: 200,
+      data: {
+        ingredient,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
-    getAllIngredients
-}
+  getAllIngredients,
+  getIngredientById,
+};
--- routes/api/ingredients.js
+++ routes/api/ingredients.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const auth = require("../../middlewares/auth");
-const { getAllIngredients } = require("../../controller/ingredientsController");
+const { getAllIngredients, getIngredientById } = require("../../controller/ingredientsController");
 
 const router = express.Router();
 
 router.get("/", auth, getAllIngredients);
+router.get("/:id", auth, getIngredientById);
 
 
 module.exports = router;
