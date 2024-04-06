f33145976bd6d8228be2eca1244f22c96149836a
Jacek Pasierb <jack_byk@o2.pl>
2024-04-06 19:46:30 +0200

    Fix/ProblemCors - finding error tets

--- routes/api/ownRecipes.js
+++ routes/api/ownRecipes.js
@@ -9,7 +9,7 @@ const {
 } = require("../../controller/ownRecipeController");
 const router = express.Router();
 const upload = require("../../middlewares/multer");
-const saveImage = require("../../controller/cloudinary/saveImg");
+const saveImages = require("../../controller/cloudinary/saveImages");
 const test = async (req, res, next)=>{
 
   res.status(201).json({
@@ -22,5 +22,5 @@ router.get("/", auth, getOwnRecipes);
 router.post("/add", auth, validateBody(recipeSchema), addOwnRecipe);
 router.delete("/remove/:recipeId", auth, deleteOwnRecipe);
 // router.post("/picture", auth, upload.single("file"), saveImage);
-router.post("/picture",auth, upload.single("file"), saveImage);
+router.post("/picture",auth, upload.single("file"), saveImages);
 module.exports = router;
