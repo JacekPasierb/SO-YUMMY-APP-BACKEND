1c0fb0ec81d39b196b8efcfc657ff5c079f9a280
Jacek Pasierb <jack_byk@o2.pl>
2024-04-06 23:16:09 +0200

    Fix/ProblemCors - finding error tets

--- controller/cloudinary/saveImages.js
+++ controller/cloudinary/saveImages.js
@@ -1,4 +1,4 @@
-const { cloudinary_js_config } = require("../../middlewares/cloudinary");
+const { cloudinary } = require("../../middlewares/cloudinary");
 
 
 const saveImages = async (req, res) => {
@@ -6,7 +6,7 @@ const saveImages = async (req, res) => {
   if (!req) {
     return res.status(400).json({ error: "Brak przesłanego pliku" });
   }
-  const result = await cloudinary_js_config.uploader.upload(req.file.path);
+  const result = await cloudinary.uploader.upload(req.file.path);
   // const result = req.file.path
   res.json(result);
 };
