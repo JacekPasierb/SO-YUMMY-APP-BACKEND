6f436bcaaae8398b0ecd060ccfbdc4e87388a5ed
Jacek Pasierb <jack_byk@o2.pl>
2024-04-07 00:05:37 +0200

    Fix/ProblemCors - finding error tets

--- middlewares/cloudinary.js
+++ middlewares/cloudinary.js
@@ -1,6 +1,10 @@
 const cloudinary = require("cloudinary").v2;
 
 const { CLOUDINARY_NAME, CLOUDINARY_KEY, CLOUDINARY_SECRET } = process.env;
+
+console.log("CLOUDINARY_NAME:", CLOUDINARY_NAME);
+console.log("CLOUDINARY_KEY:", CLOUDINARY_KEY);
+console.log("CLOUDINARY_SECRET:", CLOUDINARY_SECRET);
 cloudinary.config({
   cloud_name: CLOUDINARY_NAME,
   api_key: CLOUDINARY_KEY,
