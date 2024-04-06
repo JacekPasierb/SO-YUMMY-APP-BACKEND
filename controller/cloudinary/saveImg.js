ad819a2d26fd8b434d98b4655279847e16ec123c
Jacek Pasierb <jack_byk@o2.pl>
2024-04-06 19:42:08 +0200

    Fix/ProblemCors - finding error tets

--- controller/cloudinary/saveImg.js
+++ controller/cloudinary/saveImg.js
@@ -1,7 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 
 const saveImages = async (req, res) => {
-  const result = await cloudinary.uploader.upload(req.file.path);
+    console.log("cloud");
+//   const result = await cloudinary.uploader.upload(req.file.path);
   res.json(result);
 };
 module.exports = saveImages;
