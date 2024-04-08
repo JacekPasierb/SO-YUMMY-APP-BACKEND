2f0ca1550ddfd3303a32b0caac5374ed43940dc1
Jacek Pasierb <jack_byk@o2.pl>
2024-04-08 14:31:08 +0200

    Fix/RecipeSchema - joi

--- schemas/recipeSchema.js
+++ schemas/recipeSchema.js
@@ -1,13 +1,13 @@
 const Joi = require("joi");
 
 const recipeSchema = Joi.object({
-  preview: Joi.any().required(),
+  preview: Joi.any(),
   title: Joi.string().required(),
-  description: Joi.string().required(),
+  instructions: Joi.string().required(),
   category: Joi.string().required(),
   time: Joi.string().required(),
-  ingredients: Joi.array().items().required(),
-  instructions: Joi.string().required(),
+  ingredients: Joi.array().items(),
+  description: Joi.string().required(),
 });
 
 module.exports = recipeSchema;
