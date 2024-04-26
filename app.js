3be38b3394e64f88b4d1d40d51e795447c34b9b1
Jacek Pasierb <jack_byk@o2.pl>
2024-04-26 19:01:47 +0200

    Fix - cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.sendStatus(200);
+  next();
 
 });
 app.use(express.json());
