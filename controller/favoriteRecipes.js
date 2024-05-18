ac63bd29df6bb989f1ab9f5a79a53df97a26b623
Jacek Pasierb <jack_byk@o2.pl>
2024-05-18 14:54:47 +0200

    Future/FavoriteRecipes - add new route for remove recipe from favorites

--- controller/favoriteRecipes.js
+++ controller/favoriteRecipes.js
@@ -22,6 +22,26 @@ const addToFavorites = async (req, res, next) => {
     res.status(500).json({ message: `Recipe, ${error.message} `});
   }
 };
+
+
+const removeFromFavorite = async(req,res,next)=>{
+try {
+  const { recipeId } = req.params;
+  const userId = req.user.id;
+
+  const recipe = await Recipe.findById(recipeId);
+  if (!recipe) {
+    return res.status(404).json({ message: `Recipe not found` });
+  }
+  recipe.favorite = recipe.favorites.filter(id => id !== userId)
+  await recipe.save();
+
+  res.status(200).json({ message: `Removed from favorites successfully` });
+} catch (error) {
+  res.status(500).json({ message: `Recipe, ${error.message} `});
+}
+}
 module.exports = {
   addToFavorites,
+  removeFromFavorite
 };
