3ea99aa5df714e0f7f566161e090b24fd041f75d
Jacek Pasierb <jack_byk@o2.pl>
2024-05-18 19:52:33 +0200

    Fix/favoriteREcipes - resolved problem and change router post on get for getFavorites

--- controller/favoriteRecipes.js
+++ controller/favoriteRecipes.js
@@ -9,9 +9,9 @@ const getFavorites = async (req, res, next) => {
     res.status(200).json({
       status: "success",
       code: 200,
-      data: {
+      data: 
         favoriteRecipes,
-      },
+      
     });
   } catch (error) {
     res.status(500).json({
