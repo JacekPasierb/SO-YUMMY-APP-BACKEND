183c4b480ba82f13f84844981771f4a16e126505
Jacek Pasierb <jack_byk@o2.pl>
2024-05-18 15:04:54 +0200

    Fix/FavoriteRecipes - correct letter

--- controller/favoriteRecipes.js
+++ controller/favoriteRecipes.js
@@ -19,29 +19,28 @@ const addToFavorites = async (req, res, next) => {
 
     res.status(200).json({ message: "Recipe added to favorites", recipe });
   } catch (error) {
-    res.status(500).json({ message: `Recipe, ${error.message} `});
+    res.status(500).json({ message: `Recipe, ${error.message} ` });
   }
 };
 
+const removeFromFavorite = async (req, res, next) => {
+  try {
+    const { recipeId } = req.params;
+    const userId = req.user.id;
 
-const removeFromFavorite = async(req,res,next)=>{
-try {
-  const { recipeId } = req.params;
-  const userId = req.user.id;
+    const recipe = await Recipe.findById(recipeId);
+    if (!recipe) {
+      return res.status(404).json({ message: `Recipe not found` });
+    }
+    recipe.favorites = recipe.favorites.filter((id) => id !== userId);
+    await recipe.save();
 
-  const recipe = await Recipe.findById(recipeId);
-  if (!recipe) {
-    return res.status(404).json({ message: `Recipe not found` });
+    res.status(200).json({ message: `Removed from favorites successfully` });
+  } catch (error) {
+    res.status(500).json({ message: `Recipe, ${error.message} ` });
   }
-  recipe.favorite = recipe.favorites.filter(id => id !== userId)
-  await recipe.save();
-
-  res.status(200).json({ message: `Removed from favorites successfully` });
-} catch (error) {
-  res.status(500).json({ message: `Recipe, ${error.message} `});
-}
-}
+};
 module.exports = {
   addToFavorites,
-  removeFromFavorite
+  removeFromFavorite,
 };
