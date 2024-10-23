74c5e659f4e0ef575966d9241a34fa93d88a1c1a
Jacek Pasierb <jack_byk@o2.pl>
2024-10-23 20:14:58 +0200

    Feature/shoppingList - add endpoint for shoppingList

--- controller/shoppingListController.js
+++ controller/shoppingListController.js
@@ -1,50 +1,69 @@
 const ShoppingList = require("../models/shoppingListModel");
 
 const getShoppingList = async (req, res, next) => {
-  try {
-  } catch (error) {}
+    try {
+        const shoppingList = await ShoppingList.findOne({ userId: req.user._id });
+        
+        if (!shoppingList) {
+          return res.status(404).json({ message: "Lista zakupów nie znaleziona" });
+        }
+    
+        res.status(200).json(shoppingList);
+      } catch (error) {
+        next(error); 
+      }
 };
 
 const addIngredient = async (req, res, next) => {
-    console.log("too");
     
   try {
     const { ingredientId, thb, name, measure, recipeId } = req.body;
     const userId = req.user._id;
     let shoppingList = await ShoppingList.findOne({ userId });
-    console.log("shoppingList", shoppingList);
    
-    
-    
     if (!shoppingList) {
       shoppingList = new ShoppingList({ userId, items: [] });
     }
-console.log("Składnie", ingredientId);
-console.log("Otrzymane dane:", { ingredientId, thb, name, measure, recipeId });
     shoppingList.items.push({ ingredientId, thb, name, measure, recipeId });
-console.log("dochodzi tu?");
-try {
+
     await shoppingList.save();
-    console.log("Lista zapisano poprawnie:", shoppingList);
-} catch (err) {
-    console.log("Błąd podczas zapisywania listy zakupów:", err);
-}
-console.log("aaa",shoppingList);
 
     return res.status(201).json({
       message: "Składnik dodany do listy zakupów",
       shoppingList
     });
   } catch (error) {
-    res.status(500).json({
-      message: `Error while add ingredient to shoppingList:, ${error.message} `,
-    });
+    next(error);
   }
 };
 
 const deleteIngredient = async (req, res, next) => {
-  try {
-  } catch (error) {}
+    try {
+        const { ingredientId } = req.params;
+        const userId = req.user._id;
+    
+        
+        const shoppingList = await ShoppingList.findOne({ userId });
+    
+        if (!shoppingList) {
+          return res.status(404).json({ message: "Lista zakupów nie znaleziona" });
+        }
+    
+        shoppingList.items = shoppingList.items.filter(
+            (item) => item.ingredientId !== ingredientId
+          );
+    
+    
+        // Zapisz zmiany
+        await shoppingList.save();
+    
+        res.status(200).json({
+          message: "Składnik usunięty z listy zakupów",
+          shoppingList,
+        });
+      } catch (error) {
+        next(error); // Przekazujemy błąd dalej do middleware obsługującego błędy
+      }
 };
 
 module.exports = {
