cb103268d3f798023a3f6599016b4b1f3bb59650
Jacek Pasierb <jack_byk@o2.pl>
2024-11-18 00:48:02 +0100

    refactor: poprawki

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ const CSS_URL =
 const swaggerJsdoc = require("swagger-jsdoc");
 
 app.use(logger(formatsLogger));
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
