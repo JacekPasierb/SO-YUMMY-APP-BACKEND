55852d38af3701ff3cc7a0f7580b27d5487effa3
Jacek Pasierb <jack_byk@o2.pl>
2024-11-23 19:06:44 +0100

    Poprawa konfiguracji CORS i zwiększenie limitu rozmiaru żądania

--- src/app.ts
+++ src/app.ts
@@ -25,8 +25,8 @@ const CSS_URL =
 const swaggerJsdoc = require("swagger-jsdoc");
 
 app.use(logger(formatsLogger));
-app.use(express.json({ limit: "20mb" }));
-app.use(express.urlencoded({ limit: "20mb", extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use(express.static("public"));
 const allowedOrigins = [
